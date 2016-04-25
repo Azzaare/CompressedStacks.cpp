802bb6ad360d6bcd20a4d261a765a616e785b1e2
Azzaare <jeanfrancois.baffier@gmail.com>
2016-04-25 10:21:54 +0900

    Added CMake files

--- sources/sign.hpp
+++ sources/sign.hpp
@@ -26,11 +26,9 @@ private:
 
 /* Derived types: Block and Levels */
 // A Partially Compressed Block is composed of the signatures of its SubBlocks
-template<class T>
-using Block = std::vector<Signature<T>>;
+template<class T> using Block = std::vector<T,Signature<T>>;
 template<class T> Block<T> initBlock(int space);
 
 // Each level of compressed Blocks (first and second) are stored in Levels
-template<class T>
-using Levels = std::vector<Block<T>>;
+template<class T> using Levels = std::vector<Block<T>>;
 template<class T> Levels<T> initLevels(int space, int depth);
